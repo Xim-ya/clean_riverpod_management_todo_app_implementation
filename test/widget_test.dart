b22d9be1071e38700a18c6d4fe84f9ae7be008fd
XimYa <hj_lee_com@naver.com>
2023-12-22 17:36:14 +0900

    test : basic widget test (on todo submitted)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,67 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:clean_riverpod_management/main.dart';
+import 'package:clean_riverpod_management/src/home.dart';
+import 'package:clean_riverpod_management/src/providers/todo_list_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:clean_riverpod_management/main.dart';
+/// A testing utility which creates a [ProviderContainer] and automatically
+/// disposes it at the end of the test.
+ProviderContainer createContainer({
+  ProviderContainer? parent,
+  List<Override> overrides = const [],
+  List<ProviderObserver>? observers,
+}) {
+  // Create a ProviderContainer, and optionally allow specifying parameters.
+  final container = ProviderContainer(
+    parent: parent,
+    overrides: overrides,
+    observers: observers,
+  );
+
+  // When the test ends, dispose the container.
+  addTearDown(container.dispose);
+
+  return container;
+}
+
+class TestHome extends ConsumerWidget {
+  const TestHome({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Add new todo', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(ProviderScope(child: MyApp()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final targetWidget = Home();
+    final element = tester.element(find.byWidget(targetWidget));
+    final container = ProviderScope.containerOf(element);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    const inputText = 'New Todo';
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // await tester.enterText(find.byKey(addTodoKey), inputText);
+    // await tester.testTextInput.receiveAction(TextInputAction.done);
+
+    expect(
+      container.read(todoListProvider).last.description,
+      inputText,
+    );
+
+    // Tap the add button
+    // await tester.tap(find.byKey(addTodoKey));
+    // await tester.pump();
+
+    // Verify that the new todo is added.
+    // expect(find.text('New Todo'), findsOneWidget);
   });
 }
